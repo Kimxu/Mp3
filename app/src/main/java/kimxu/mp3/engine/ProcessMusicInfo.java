0636ac53e0535cc17cc895909587a2d2dd0f0baf
kimxu <kimxu_me@163.com>
2015-12-10 21:51:34 +0800

    first commit

---
+++ app/src/main/java/kimxu/mp3/engine/ProcessMusicInfo.java
@@ -0,0 +1,9 @@
+package kimxu.mp3.engine;
+
+
+/**
+ * liteplayer by loader
+ * @author qibin
+ */
+public class ProcessMusicInfo {
+}
---
+++ app/src/main/java/kimxu/mp3/utils/MobileUtils.java
@@ -0,0 +1,26 @@
+package kimxu.mp3.utils;
+
+import kimxu.mp3.application.App;
+
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+/**
+ * liteplayer by loader
+ * @author qibin
+ */
+public class MobileUtils {
+	/**
+	 * 隐藏输入法软键盘
+	 * @param view attachview
+	 * @see {@link InputMethodManager.hideSoftInputFromWindow}
+	 */
+	public static void hideInputMethod(View view) {
+		InputMethodManager imm = (InputMethodManager) App.sContext
+				.getSystemService(Context.INPUT_METHOD_SERVICE);
+		if(imm.isActive()) {
+			imm.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+		}
+	}
+}
